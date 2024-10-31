c1181efd4296f962001becf694ee4968b15399f0
Kyle <kimberg@madisoncollege.edu>
2024-10-31 15:25:52 -0500

    Updated welcome message in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, Kyle!</h1>
+        <h1>Hello, David!</h1>
         <p>
           Welcome to my React application.
         </p>
